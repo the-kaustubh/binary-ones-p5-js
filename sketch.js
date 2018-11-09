859a663d9c963afdf844a12876ac980ccaff895d
the-kaustubh <kaustubh.murumkar@gmail.com>
2018-11-10 00:27:39 +0530

    Clean Up

--- sketch.js
+++ sketch.js
@@ -4,17 +4,15 @@ let yPoints = [];
 let xCounter = 0;
 
 function setup() {
+  
   createCanvas(800, 600);
   background(0);
   textAlign(CENTER, CENTER);
-  // x axis
 
   stroke(255);
   line(0, 500, width-20, 500);
   strokeWeight(8);
 
-
-  // xCounter=200;
   for(xInc = 25; xInc < width-15; xInc+=25) {
     xPoints.push(xInc);
     yPoints.push(500 - (getOnes(xCounter)*25));
@@ -47,18 +45,15 @@ function setup() {
 
 function draw() {
   noLoop();
-  // beginShape();
   for(i = 0; i < xPoints.length; i ++) {
     push();
     strokeWeight(4);
     point(xPoints[i], yPoints[i]);
     pop();
-    console.log(xPoints[i] + ", " + yPoints[i]);
   }
   for(j = 0; j < xPoints.length; j++) {
     line(xPoints[j], yPoints[j], xPoints[j+1], yPoints[j+1]);
   }
-  // endShape();
 }
 
 function getOnes(vble)  {
